a8ded0fa5e1a687f70be214f3dcc5681a94fef84
Hyejeong <thswjd0609@gmail.com>
2025-05-18 02:30:30 +0900

    [#6] feat: 회원 가입 정보 추가

--- src/lib/schemas/auth.ts
+++ src/lib/schemas/auth.ts
@@ -23,6 +23,15 @@ export const SignUpSchema = z.object({
     .regex(/[\W_]/, {
       message: "패스워드는 최소 1개 이상의 특수문자를 포함해야 합니다.",
     }),
+  phone: z.string().regex(/^010-\d{4}-\d{4}$/, {
+    message: "전화번호는 010-XXXX-XXXX 형식이어야 합니다.",
+  }),
+  birth: z.string().refine((val) => !isNaN(Date.parse(val)), {
+    message: "생년월일을 선택해주세요.",
+  }),
+  gender: z.enum(["male", "female"], {
+    message: "성별을 선택해주세요.",
+  }),
 });
 
 export const LoginSchema = z.object({
