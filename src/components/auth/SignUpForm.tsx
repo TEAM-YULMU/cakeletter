71dd878d56efb1689856952df3dfb6f434a46db0
Hyejeong <thswjd0609@gmail.com>
2025-05-18 02:30:30 +0900

    [#6] feat: 회원 가입 정보 추가

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -9,6 +9,7 @@ import { useFormValidate } from "@/hooks/useFormValidate";
 import { TSignUpFormError } from "@/types/form";
 import { SignUpSchema } from "@/lib/schemas/auth";
 import { FormMessage } from "./FormMessage";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 
 type Props = {
   role: "user" | "owner";
@@ -32,23 +33,56 @@ export function SignUpForm({ role }: Props) {
     >
       <form className="space-y-6">
         {/* 이름 */}
-        <div className="space-y-1">
+        <div className="space-y-2">
           <Label htmlFor="name">이름</Label>
           <Input id="name" name="name" placeholder="이름을 입력해주세요" onChange={handleChange} />
           {errors?.name && <FormMessage message={errors?.name[0]} />}
         </div>
+
         {/* 이메일 */}
-        <div className="space-y-1">
+        <div className="space-y-2">
           <Label htmlFor="email">이메일</Label>
           <Input id="email" name="email" type="email" placeholder="example@example.com" onChange={handleChange} />
           {errors?.email && <FormMessage message={errors?.email[0]} />}
         </div>
+
         {/* 비밀번호 */}
-        <div className="space-y-1">
+        <div className="space-y-2">
           <Label htmlFor="password">비밀번호</Label>
           <Input id="password" name="password" type="password" placeholder="비밀번호를 입력해주세요" onChange={handleChange} />
           {errors?.password && <FormMessage message={errors?.password[0]} />}
         </div>
+
+        {/* 전화번호 */}
+        <div className="space-y-2">
+          <Label htmlFor="phone">전화번호</Label>
+          <Input id="phone" name="phone" placeholder="010-XXXX-XXXX" onChange={handleChange} />
+          {errors?.phone && <FormMessage message={errors.phone[0]} />}
+        </div>
+
+        {/* 생년월일 */}
+        <div className="space-y-2">
+          <Label htmlFor="birth">생년월일</Label>
+          <Input id="birth" name="birth" type="date" onChange={handleChange} />
+          {errors?.birth && <FormMessage message={errors.birth[0]} />}
+        </div>
+
+        {/* 성별 */}
+        <div className="space-y-2">
+          <Label htmlFor="gender">성별</Label>
+          <RadioGroup name="gender" defaultValue="male" className="flex gap-4" onValueChange={(value) => validateField("gender", value)}>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="male" id="male" />
+              <Label htmlFor="male">남자</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="female" id="female" />
+              <Label htmlFor="female">여자</Label>
+            </div>
+          </RadioGroup>
+          {errors?.gender && <FormMessage message={errors.gender[0]} />}
+        </div>
+
         <Submit className="w-full" role={role}>
           가입하기
         </Submit>
--- src/types/form.ts
+++ src/types/form.ts
@@ -2,6 +2,9 @@ export type TSignUpFormError = {
   name?: string[];
   email?: string[];
   password?: string[];
+  phone?: string[];
+  birth?: string[];
+  gender?: string[];
 };
 
 export type TLoginFormError = {
