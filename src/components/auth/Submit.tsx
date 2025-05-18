84ce72897975edc2dc9690cb359fcc2b362dbe4d
Hyejeong <thswjd0609@gmail.com>
2025-05-18 19:53:23 +0900

    [#6] feat: jwt 기반 세션 관리 및 로그인 인증 처리 구현

--- src/components/auth/Submit.tsx
+++ src/components/auth/Submit.tsx
@@ -1,17 +1,19 @@
 import { Button, type ButtonProps } from "../ui/button";
+import { useFormStatus } from "react-dom";
 
 type SubmitProps = ButtonProps & {
-  role: "user" | "owner";
+  role?: "user" | "owner"; // optional
 };
 
-export function Submit({ children, role, className, ...others }: SubmitProps) {
+export function Submit({ children, role = "user", className = "", ...others }: SubmitProps) {
+  const { pending } = useFormStatus();
   const colorVariants = {
-    user: "color-primary-300 hover:bg-pink-600 text-white",
-    owner: "color-secondary-300 hover:bg-green-600 text-white",
+    user: "color-primary-300 hover:bg-pink-500 text-white",
+    owner: "color-secondary-300 hover:bg-green-500 text-white",
   };
 
   return (
-    <Button type="submit" className={`${className} ${colorVariants[role]}`} {...others}>
+    <Button type="submit" disabled={pending} className={`${colorVariants[role]} ${className}`} {...others}>
       {children}
     </Button>
   );
