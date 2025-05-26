c97fe216a65b4437042468800e9b94c8129e3c47
Hyejeong <thswjd0609@gmail.com>
2025-05-26 22:44:29 +0900

    [#55] feat: middleware 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,29 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 import { verify } from "./lib/actions/sessions";
-import { AUTH_ROUTES, BASE_URL, PUBLIC_ROUTES } from "./constants/routes";
+import { BASE_URL, PUBLIC_ROUTES, PROTECTED_ROUTES } from "./constants/routes";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
   const cookieStore = await cookies();
-
   const cookie = cookieStore.get("session")?.value;
   const session = await verify(cookie);
 
-  if (!isPublicRoute && !session) {
-    return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, request.nextUrl));
+  const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
+  const isChatRoute = pathname.startsWith(PROTECTED_ROUTES.CHAT);
+  const isOwnerRoute = pathname.startsWith(PROTECTED_ROUTES.OWNER);
+
+  // 인증되지 않은 사용자가 보호된 경로 접근 시
+  if ((isChatRoute || isOwnerRoute) && !session) {
+    return NextResponse.redirect(new URL(BASE_URL, request.url));
   }
 
+  // 인증된 일반 사용자가 OWNER 경로 접근 시
+  if (isOwnerRoute && session?.role !== "OWNER") {
+    return NextResponse.redirect(new URL(BASE_URL, request.url));
+  }
+
+  // 로그인 중인데 로그인/회원가입 페이지 접근 시
   if (isPublicRoute && session) {
     return NextResponse.redirect(new URL(BASE_URL, request.nextUrl));
   }
@@ -23,19 +32,6 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// login, signup의 경우에만 middleware.ts 거치도록 설정
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico
-     */
-    // '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    "/login",
-    "/signup/user",
-    "/signup/owner",
-  ],
+  matcher: ["/login", "/signup/user", "/signup/owner", "/chat/:path*", "/owner/:path*"],
 };
