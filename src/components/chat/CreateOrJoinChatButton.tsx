736048a48a27d87ac18b14d3e8b2b9bbdfaac026
Hyejeong <thswjd0609@gmail.com>
2025-05-26 16:11:25 +0900

    [#48] fix: 본인과의 채팅 예외 처리 임시 alert 추가

--- src/components/chat/CreateOrJoinChatButton.tsx
+++ src/components/chat/CreateOrJoinChatButton.tsx
@@ -27,10 +27,11 @@ export default function CreateOrJoinChatButton({ storeId, className, variant, ch
       if (data.ok && data.ok && data.roomId) {
         router.push(`/chat/${data.roomId}`); // 채팅 페이지로 이동
       } else {
-        throw new Error(data.message || "채팅방 생성에 실패했습니다.");
+        alert(data.message);
       }
     } catch (error) {
       console.error("채팅방 생성 오류:", error);
+      alert("문제가 발생했습니다. 다시 시도해주세요.");
     }
   };
 
