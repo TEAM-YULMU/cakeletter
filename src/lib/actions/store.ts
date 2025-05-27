c792e0ad863035bbb36c429de1af9d902bdf538b
jhpark0888 <jhpark0888@naver.com>
2025-05-27 15:32:03 +0900

    [#60] refactor: 상품 수정 화면 접근 시 자신의 상품 아닐 경우 접근 불가능하게 처리

--- src/lib/actions/store.ts
+++ src/lib/actions/store.ts
@@ -1,5 +1,20 @@
 import { prisma } from "@/lib/prisma";
 import { StoreDetail } from "@/types/store";
+import { verifySession } from "./sessions";
+
+export const getMyStoreId = async () => {
+  const session = await verifySession();
+  if (!session) return;
+
+  const storeId = await prisma.store.findUnique({
+    where: { memberId: +session.id },
+    select: {
+      id: true,
+    },
+  });
+
+  return storeId?.id;
+};
 
 export const getStore = async (storeId: number): Promise<StoreDetail> => {
   const storeData = await prisma.store.findUnique({
